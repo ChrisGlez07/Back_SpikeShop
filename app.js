e84d5564fca9739fe3f03be1294c97a351e872a9
ChrisGlez07 <crisglez0718@gmail.com>
2025-11-04 15:08:39 -0600

    Products Crud Correction

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import db from './src/Config/db.js';
 import userRoutes from './src/Routes/user.route.js';
+import productoRoutes from './src/Routes/producto.route.js';
 import { generateTokenGeneric } from './src/Helpers/auth.js';
 
 const connectDB = db;
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/productos', productoRoutes);
 app.listen(port, () => {
     console.log('Generando token generico:');
     console.log(generateTokenGeneric())
--- src/Controllers/producto.controller.js
+++ src/Controllers/producto.controller.js
@@ -1,9 +1,9 @@
-import * as producto from '../Services/producto.service.js';
+import * as producto from '../Service/producto.service.js';
 
 export async function createProducto(req, res, next) {  
     try {
-        const { nombre, tipo, cantidad, precio } = req.body;
-        const newProducto = { nombre, tipo, cantidad, precio };
+        const { nombre, tipo, cantidad, precio, imagen } = req.body;
+        const newProducto = { nombre, tipo, cantidad, precio, imagen };
         const productoCreated = await producto.createProducto(newProducto);
         res.status(201).json({
             message: "Producto creado exitosamente",
