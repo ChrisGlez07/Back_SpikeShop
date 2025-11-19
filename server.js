e73fdce9b368080178616d56d41e5f02ee1cbb73
Eduardo-gif-beep <st4111@utr.edu.mx>
2025-11-19 10:39:25 -0600

    mensaje secreto

---
+++ server.js
@@ -0,0 +1,15 @@
+//querido diario, el chris me dijo que para hacer que solo los admin puedan hacer ciertas acciones
+//tenia que crear una funcion isAdmin en auth.js y usarla como middleware en las rutas que quiero proteger
+//asi que eso hice, y ahora solo los admin pueden acceder a esas rutas protegidas
+//espero que esto funcione bien y no haya problemas despues
+//fin del diario
+
+//ademas, estoy orgulloso del memo, y de todo mi equipo de desarrollo, que han trabajado duro para mejorar la seguridad
+//y la funcionalidad de nuestra aplicacion. seguimos aprendiendo y creciendo juntos.
+//gracias por todo el apoyo y la paciencia.
+
+//hasta la proxima entrada del diario!
+
+//chris es gay y memo es su novio secreto 
+
+//atte yourdevteam
